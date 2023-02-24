2b20527b59b0ba0efe815f3d8268a1620dc506b6
Emanuel Nzinga Maimona <emanuelmaimona@gmail.com>
2023-02-24 19:49:25 +0100

    publish site

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,12 @@ import React from 'react'
 export default function Header() {
     return (
         <header className='header'>
-            
+            <div className='logo'>
+                <img src='./assets/images/esport_manager.png' alt='logo'></img>
+            </div>
+            <div className='log'>
+                
+            </div>
         </header>
     )
 }
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Main() {
+    return (
+        <header className='header'>
+            
+        </header>
+    )
+}
