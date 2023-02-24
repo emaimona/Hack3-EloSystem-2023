a3467f334d768e27722b6a58b71fe1188a9b34fe
Emanuel Nzinga Maimona <emanuelmaimona@gmail.com>
2023-02-24 19:40:22 +0100

    update app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Header from './components/Header'
-import Main from './components/Header'
-import Footer from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import './style.css'
 
 export default function App() {
     return (
